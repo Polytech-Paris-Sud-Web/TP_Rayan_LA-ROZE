26365fc5f3ced8bdcb2d3e22565e6fd9c531140d
Rayan LA ROZE <rayan.la-roze@u-psud.fr>
2021-03-16 08:43:54 +0100

    Rendu final

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Article} from '../models/article';
 import { ArticleService } from '../article.service';
 import { Output, EventEmitter } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-article',
@@ -15,20 +15,25 @@ export class ArticleComponent implements OnInit {
 	article: Article;
 	@Output() 
 	deleteArticle: EventEmitter<Article> = new EventEmitter<Article>();
+	id: number | null; 
 
 	constructor(private articleService: ArticleService, 
-		private route: ActivatedRoute) { }
+		private route: ActivatedRoute, private router: Router) { }
 
 	ngOnInit() {
-		const id = this.route.snapshot.params['id'];
-		if(id){
-			this.articleService.getArticle(id).subscribe(data=>{
+		this.id = this.route.snapshot.params['id'];
+		if(this.id){
+			this.articleService.getArticle(this.id).subscribe(data=>{
 				this.article = data;
 			});
 		}
 	}
 
 	removeArticle(){
-		this.deleteArticle.emit(this.article);
+		if(this.id){
+			this.articleService.deleteArticle(this.article.id).subscribe((data)=>{
+				this.router.navigate(['/articles']);
+			});
+		}else this.deleteArticle.emit(this.article);
 	}
 }
